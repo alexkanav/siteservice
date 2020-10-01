af9d050eac926306cc33880e85bb976c3da6b579
root <root@alex.alex>
2020-10-01 17:17:59 +0300

    Init

---
+++ glavnaya/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+     
+
+def index(request):
+
+    return HttpResponse('<h2>Hello, world. You\'re at the page 1.</h2>')
